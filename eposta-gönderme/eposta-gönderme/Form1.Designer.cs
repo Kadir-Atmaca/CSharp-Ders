c7227645943ed4025415b87ac2125e9392bef80a
Kadir Atmaca <152689373+Kadir-Atmaca@users.noreply.github.com>
2024-02-04 21:13:12 +0300

    Add files via upload

---
+++ eposta-gönderme/eposta-gönderme/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace eposta_gönderme
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.Konu = new System.Windows.Forms.TextBox();
+            this.altBaslik = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.button1.ForeColor = System.Drawing.Color.Red;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(776, 201);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Gönder";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Konu
+            // 
+            this.Konu.Location = new System.Drawing.Point(12, 219);
+            this.Konu.Multiline = true;
+            this.Konu.Name = "Konu";
+            this.Konu.Size = new System.Drawing.Size(775, 69);
+            this.Konu.TabIndex = 1;
+            // 
+            // altBaslik
+            // 
+            this.altBaslik.Location = new System.Drawing.Point(13, 294);
+            this.altBaslik.Multiline = true;
+            this.altBaslik.Name = "altBaslik";
+            this.altBaslik.Size = new System.Drawing.Size(775, 72);
+            this.altBaslik.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(9, 369);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(253, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "https://www.youtube.com/@kadiratmaca1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 391);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.altBaslik);
+            this.Controls.Add(this.Konu);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox Konu;
+        private System.Windows.Forms.TextBox altBaslik;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ eposta-gönderme/eposta-gönderme/Form1.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.Mail;
+
+
+//https://www.youtube.com/@kadiratmaca1
+namespace eposta_gönderme
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Bu kısma Gönderici Epostası ve şifresini giriyoruz doğru şekilde uyarı outlook eposta olmalı
+            string GöndericiEposta = "outlook hesabı örnek gönderici@outlook.com ";
+            string GöndericiEpostaSifre = "outlook hesabının şifresi ";
+            //Bu kısmafa göndermek istediyimiz epostayı giriyoruz burada farketmez istediyinizi girin 
+            string AlıcıEposta = "alıcının eposta adresi örnek alıcı@gmail.com";
+
+
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress(GöndericiEposta);
+            mail.To.Add(AlıcıEposta);
+
+
+
+            mail.Subject = Konu.Text;  // burası epostanın konusudur
+            mail.Body = altBaslik.Text; // burası epostanın açıklamasıdır 
+            //Burada Resim zip rar v.b şeyler gönderebilirsiniz burad aynı kodla birden fazlada dosya gönderbilirsiniz.
+            mail.Attachments.Add(new Attachment("1.jpg"));
+           // mail.Attachments.Add(new Attachment("2.zip"));
+
+
+
+            SmtpClient smtp = new SmtpClient("smtp-mail.outlook.com",587);
+            smtp.Credentials = new System.Net.NetworkCredential(GöndericiEposta, GöndericiEpostaSifre);
+            smtp.EnableSsl = true;
+            smtp.Send(mail);
+        }
+    }
+}
